7a3be15162d73400050da469c7d69c60673bdf79
Bárbara <babyr.oliveira@gmail.com>
2020-06-17 20:53:41 -0300

    Resolução do exercício.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Sinal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double numero; 
+
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+                    Console.WriteLine(" -----  Negativo, Zero ou Positivo ----- \n");
+                            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.Write("Digite um número: ");
+                           numero = Convert.ToDouble(Console.ReadLine());
+
+            if (numero >= 1 )
+                Console.WriteLine("Positivo.");
+
+            else if (numero <=-1)
+                     Console.WriteLine("Negativo.");
+
+            else if (numero == 0)
+                     Console.WriteLine("Zero.");
+
+            Console.ReadKey();
+
+        }
+    }
+}
